6c8556bbcbd48f86e097d638c92018cccc84ddd6
Zipra1 <zippyzipra@gmail.com>
2023-03-23 09:13:59 +0000

    Main

--- cat.py
+++ cat.py
@@ -7,4 +7,29 @@ class Cat:
         self.colour = colour
         self.is_hungry = choice([True,False])
         self.want_to_scratch = choice([True,False])
-        
+    def eat(self, food_name):
+        if self.is_hungry:
+            print(f'{self.name} ate {food_name}')
+            self.is_hungry = False
+        else:
+            print(f'{self.name} is not hungry.')
+    def scratch(self, post):
+        if self.want_to_scratch:
+            if post.use():
+                self.wants_to_scratch = False
+                print(f'{self.name} scratched {post}.')
+            else:
+                print(f'{post} is dead.')
+        else:
+            print(f'{self.name} does not want to scratch.')
+
+class ScratchPost:
+    def __init__(self):
+        self.hp = 10
+    
+    def use(self):
+        if self.hp <= 0:
+            return False
+        else:
+            self.hp -= 1
+            return True
---
+++ main.py
@@ -0,0 +1,12 @@
+from cat import Cat
+
+cat1=Cat('Cheetos','Tabby','Orange')
+print(cat1)
+print(cat1.name,cat1.breed,cat1.colour) # Accessing an attribute
+
+if cat1.is_hungry:
+    print(f'{cat1.name} is hungry.') # f means formatting
+    cat1.eat('Food?')
+else:
+    print(f'{cat1.name} is not hungry.')
+    cat1.eat('Food?')
